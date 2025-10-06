9e6d0fcc01660b0b2c7316826b99f1ba258d7afa
katarzynakaz <l453r3@gmail.com>
2025-10-06 18:51:20 +0100

    form partially done

---
+++ form.mjs
@@ -0,0 +1,30 @@
+import { addData } from "./storage.mjs";
+
+//to submit the form
+const selectedUser = document.querySelector("#userSelection");
+const topicName = document.querySelector("#topicName");
+const submitBtn = document.querySelector(".btn");
+const firstDate = document.querySelector("#firstDate");
+
+export function selectDate() {
+	// const firstDate = () => new Date().toLocaleDateString() here this creates d/m/yyyy output which is compatible with the browser but not with input type date in form
+	//so I am sticking to below method
+	firstDate.value = new Date().toISOString().split("T")[0]; //this creates yyyy-mm-dd output which is compatible with input type date in form
+}
+
+submitBtn.addEventListener("click", () => {
+	const selectedUserValue = selectedUser.value;
+	const topicNameValue = topicName.value;
+	const firstDateValue = firstDate.value;
+
+	if (selectedUserValue === "default" || !firstDateValue || !topicNameValue) {
+		alert("Please fill all the fields");
+		return;
+	}
+	const newEntry = {
+		topic: topicNameValue,
+		date: firstDateValue,
+	};
+	addData(selectedUserValue, [newEntry]);
+	alert("Topic added");
+});
