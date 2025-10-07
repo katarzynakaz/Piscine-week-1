0b50bb71213bcf359e11b42a185c2622b473725e
katarzynakaz <l453r3@gmail.com>
2025-10-07 11:17:59 +0100

    finished form

--- form.mjs
+++ form.mjs
@@ -13,7 +13,33 @@ export function selectDate() {
 	firstDate.value = new Date().toISOString().split("T")[0]; //this creates yyyy-mm-dd output which is compatible with input type date in form
 }
 
-submitBtn.addEventListener("click", () => {
+//this was the way I did it initially, and to make form submit on enter I am using submit event inbuild into form
+// submitBtn.addEventListener("click", (event) => {
+// 	event.preventDefault(); // added this to show message topic added which was not showing because of the instant page reload on form submission
+
+// 	const selectedUserValue = selectedUser.value;
+// 	const topicNameValue = topicName.value;
+// 	const firstDateValue = firstDate.value;
+
+// 	if (selectedUserValue === "default" || !firstDateValue || !topicNameValue) {
+// 		alert("Please fill all the fields");
+// 		return;
+// 	}
+// 	const newEntry = {
+// 		topic: topicNameValue,
+// 		date: firstDateValue,
+// 	};
+// 	addData(selectedUserValue, [newEntry]);
+// 	topicAdded.innerHTML = "Topic added";
+// });
+
+// so this becomes:
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+	// instead of click it is submit event
+	event.preventDefault(); // added this to show message topic added which was not showing because of the instant page reload on form submission
+
 	const selectedUserValue = selectedUser.value;
 	const topicNameValue = topicName.value;
 	const firstDateValue = firstDate.value;
