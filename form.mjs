efe999b32472b728479389185aeefada5c9bd34a
Nataliia74 <volkov.nataliia@gmail.com>
2025-10-08 20:47:27 +0100

    "handle the conflict"

--- form.mjs
+++ form.mjs
@@ -9,9 +9,9 @@ const firstDate = document.querySelector("#firstDate");
 const topicAdded = document.querySelector("#topicAdded");
 
 export function selectDate() {
-	// const firstDate = () => new Date().toLocaleDateString() here this creates d/m/yyyy output which is compatible with the browser but not with input type date in form
-	//so I am sticking to below method
-	firstDate.value = new Date().toISOString().split("T")[0]; //this creates yyyy-mm-dd output which is compatible with input type date in form
+  // const firstDate = () => new Date().toLocaleDateString() here this creates d/m/yyyy output which is compatible with the browser but not with input type date in form
+  //so I am sticking to below method
+  firstDate.value = new Date().toISOString().split("T")[0]; //this creates yyyy-mm-dd output which is compatible with input type date in form
 }
 
 //this was the way I did it initially, and to make form submit on enter I am using submit event inbuild into form
@@ -50,42 +50,42 @@ const form = document.querySelector("form");
 const displayAgenda = document.querySelector("#displayAgendaBox");
 
 form.addEventListener("submit", (event) => {
-	// instead of click it is submit event
-	event.preventDefault(); // added this to show message topic added which was not showing because of the instant page reload on form submission
+  // instead of click it is submit event
+  event.preventDefault(); // added this to show message topic added which was not showing because of the instant page reload on form submission
 
-	const selectedUserValue = selectedUser.value;
-	// let topicNameValue = topicName.value;
-	let topicNameValue = topicName.value.trim().toUpperCase();
+  const selectedUserValue = selectedUser.value;
+  // let topicNameValue = topicName.value;
+  let topicNameValue = topicName.value.trim().toUpperCase();
 
-	const firstDateValue = firstDate.value;
+  const firstDateValue = firstDate.value;
 
-	if (
-		selectedUserValue === "default" ||
-		!firstDateValue ||
-		!topicNameValue ||
-		topicNameValue === ""
-	) {
-		alert("Please fill all the fields");
-		return;
-	}
+  if (
+    selectedUserValue === "default" ||
+    !firstDateValue ||
+    !topicNameValue ||
+    topicNameValue === ""
+  ) {
+    alert("Please fill all the fields");
+    return;
+  }
 
-	const newEntry = {
-		topic: topicNameValue,
-		date: firstDateValue,
-	};
-	addData(selectedUserValue, [newEntry]);
-	topicAdded.innerHTML = "Topic added";
+  const newEntry = {
+    topic: topicNameValue,
+    date: firstDateValue,
+  };
+  addData(selectedUserValue, [newEntry]);
+  topicAdded.innerHTML = "Topic added";
 
-	// cleaned displayAgenda space and render updated an Agenda after submitting a new topic to the Agenda
-	displayAgenda.innerHTML = "";
-	const updatedData = getData(selectedUserValue);
-	if (updatedData && updatedData.length > 0) {
-		const currentDate = new Date().toISOString().split("T")[0];
-		const futureDate = updatedData.filter((entry) => entry.date >= currentDate);
-		renderAgenda(futureDate);
-	}
-	// cleaned the form
-	document.querySelector("#userSelection").value = "default";
-	document.querySelector("#topicName").value = "";
-	selectDate();
+  // cleaned displayAgenda space and render updated an Agenda after submitting a new topic to the Agenda
+  displayAgenda.innerHTML = "";
+  const updatedData = getData(selectedUserValue);
+  if (updatedData && updatedData.length > 0) {
+    const currentDate = new Date().toISOString().split("T")[0];
+    const futureDate = updatedData.filter((entry) => entry.date >= currentDate);
+    renderAgenda(futureDate);
+  }
+  // cleaned the form
+  document.querySelector("#userSelection").value = "default";
+  document.querySelector("#topicName").value = "";
+  selectDate();
 });
